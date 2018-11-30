563027ed88b52d56a94e7c0f89f669bdd640f82d
Roberta Takenaka <takenaka.roberta@gmail.com>
2018-11-29 23:39:10 -0200

    v0.20

--- spskit/reception/reception.py
+++ spskit/reception/reception.py
@@ -7,7 +7,8 @@ import plumber
 
 
 from spskit.utils.file_utils import FileInfo
-import spskit.sps.pkg_files as package
+from spskit.sps.package import get_xml_packages
+from spskit.sps.article_packages import get_articles_packages
 
 
 class Reception:
@@ -51,11 +52,11 @@ class Reception:
     class get_xml_packages_pipe(plumber.Pipe):
         def transform(self, data):
             file_info_items = data
-            xml_packages, invalid_files = package.get_xml_packages(file_info_items)
+            xml_packages, invalid_files = get_xml_packages(file_info_items)
             return xml_packages, invalid_files
 
     class get_article_packages_pipe(plumber.Pipe):
         def transform(self, data):
             xml_packages, invalid_files = data
-            pkg_articles = package.get_articles_packages(xml_packages)
-            return pkg_articles, invalid_files
+            article_packages = get_articles_packages(xml_packages)
+            return article_packages
