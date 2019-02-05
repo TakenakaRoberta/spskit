4bf3f30c215847e1d9884a05c99ad3154d4ecd7f
robertatakenaka <roberta.takenaka@scielo.org>
2019-02-05 17:03:12 -0200

    v0.23

--- spskit/frontdesk/reception.py
+++ spskit/frontdesk/reception.py
@@ -9,22 +9,28 @@ from spskit.utils.xml_utils import XML
 from spskit.sps.document_data import DocumentData
 
 
-def destinate_files(files, destination_path, delete):
-    _files = []
-    for f in files:
+def get_file_info_items(received_files, destination_path, delete):
+    input_files = []
+    for f in received_files:
         file_path = os.path.join(destination_path, os.path.basename(f))
         shutil.copy(f, file_path)
-        _files.append(FileInfo(file_path))
+        input_files.append(FileInfo(file_path))
         if delete:
             try:
                 os.unlink(f)
             except:
                 pass
-    return _files
+    return input_files
 
 
 def get_xml_packages(file_info_items):
-    xml_prefixes = [f.name_prefix for f in file_info_items if f.ext == '.xml']
+    """
+    A partir de uma lista de FileInfo, retorna uma lista de pacotes XML e
+    uma lista de arquivos que não fazem parte de nenhum pacote XML
+    Param file_info_items: lista de FileInfo
+    Retorna (xml_packages, invalid_files)
+    """
+    xml_prefixes = [f.name_prefix for f in file_info_items if f.ext == ".xml"]
 
     xml_packages = {}
     invalid_files = []
@@ -32,48 +38,52 @@ def get_xml_packages(file_info_items):
     for file_info in file_info_items:
         prefix = file_info.name_prefix
         if prefix not in xml_prefixes:
-            prefix = prefix[:prefix.rfind('-')]
+            prefix = prefix[: prefix.rfind("-")]
         if prefix in xml_prefixes:
             if xml_packages.get(prefix) is None:
                 xml_packages[prefix] = {}
-                xml_packages[prefix]['name'] = prefix
-                xml_packages[prefix]['xml_file'] = None
-                xml_packages[prefix]['related_files'] = []
-            if file_info.ext == '.xml':
-                xml_packages[prefix]['xml_file'] = file_info.file_path
+                xml_packages[prefix]["name"] = prefix
+                xml_packages[prefix]["xml_file"] = None
+                xml_packages[prefix]["related_files"] = []
+            if file_info.ext == ".xml":
+                xml_packages[prefix]["xml_file"] = file_info.file_path
             else:
-                xml_packages[prefix]['related_files'].append(file_info.file_path)
+                xml_packages[prefix]["related_files"].append(file_info.file_path)
         else:
             invalid_files.append(file_info.file_path)
-    xml_packages = [xml_packages[k]
-                    for k in sorted(xml_packages.keys())]
+    xml_packages = [xml_packages[k] for k in sorted(xml_packages.keys())]
     return xml_packages, invalid_files
 
 
 def get_document_packages(xml_packages):
+    """
+    A partir de xml_packages (um dicionário de pacotes de XML),
+    retorna document_packages (o mesmo dicionário acrescido de ativos digitais,
+    dados do documento, arquivos anexos, etc)
+    """
     document_packages = []
     for xml_pkg in xml_packages:
-        files = xml_pkg['related_files']
-        xml_pkg['xml'] = XML(xml_pkg['xml_file'])
-        document_data = DocumentData(xml_pkg['xml'], xml_pkg['xml'].file_info.name_prefix)
-        xml_pkg['data'] = document_data
+        files = xml_pkg["related_files"]
+        xml_pkg["xml"] = XML(xml_pkg["xml_file"])
+        xml_pkg["data"] = DocumentData(
+            xml_pkg["xml"], xml_pkg["xml"].file_info.name_prefix
+        )
         assets = []
         attachments = []
         for f in files:
             basename = os.path.basename(f)
-            if basename in document_data.internal_xlink_href:
+            if basename in xml_pkg["data"].internal_xlink_href:
                 assets.append(f)
             else:
                 attachments.append(f)
-        xml_pkg['assets'] = assets
-        xml_pkg['attachments'] = attachments
-        xml_pkg['content'] = xml_pkg['xml'].text
-        
+        xml_pkg["assets"] = assets
+        xml_pkg["attachments"] = attachments
         document_packages.append((xml_pkg))
     return document_packages
 
 
-def receive_files(files, destination_path, delete=False):
-    file_info_items = destinate_files(files)
+def receive_files(input_files, destination_path, delete=False):
+    file_info_items = get_file_info_items(
+        input_files, destination_path, delete)
     xml_packages, invalid_files = get_xml_packages(file_info_items)
     document_packages = get_documents_packages(xml_packages)
--- tests/test_frontdesk_reception.py
+++ tests/test_frontdesk_reception.py
@@ -2,10 +2,60 @@ import unittest
 import os
 
 import spskit.frontdesk.reception as reception
+from spskit.sps.document_data import DocumentData
 from spskit.utils.files_utils import FileInfo
+from spskit.utils.xml_utils import XML
 
 
+TEST_PATH = os.path.dirname(os.path.abspath(__file__))
+PKG_PATH = os.path.join(TEST_PATH, 'fixtures/markup_xml/scielo_package')
 FIXTURES_PATH = os.path.dirname(os.path.abspath(__file__))
+
+
+class GetDocumentPackagesTest(unittest.TestCase):
+
+    def get_file_info_items(self, files):
+        return [FileInfo(f) for f in files]
+
+    def test_package_file_1(self):
+        xml_file_path = os.path.join(PKG_PATH, '0034-8910-rsp-48-01-0001.xml')
+        xml_related_files = [
+            os.path.join(PKG_PATH, '0034-8910-rsp-48-01-0001.pdf'),
+            os.path.join(PKG_PATH, '0034-8910-rsp-48-01-0001-en.pdf'),
+        ]
+        xml_pkg_items = [
+            {
+                'xml_file': xml_file_path,
+                'related_files': xml_related_files,
+            }
+        ]
+        article_pkg_items = reception.get_document_packages(xml_pkg_items)
+        result = article_pkg_items
+        xml = XML(xml_file_path)
+        expected = [
+            {
+                'name': '0034-8910-rsp-48-01-0001',
+                'xml_file': xml_file_path,
+                'content': xml.text,
+                'xml': xml,
+                'data': DocumentData(xml, 'name'),
+                'related_files': xml_related_files,
+                'assets': [],
+                'attachments': xml_related_files
+            }
+        ]
+        self.assertEqual(
+            result[0]['xml_file'], expected[0]['xml_file'])
+        self.assertEqual(
+            result[0]['related_files'], expected[0]['related_files'])
+        self.assertEqual(
+            result[0]['assets'], expected[0]['assets'])
+        self.assertEqual(
+            result[0]['attachments'], expected[0]['attachments'])
+        self.assertEqual(
+            result[0]['xml'].text, expected[0]['xml'].text)
+        self.assertIsInstance(
+            result[0]['data'], DocumentData)
 
 
 class GetXMLPackagesTest(unittest.TestCase):
